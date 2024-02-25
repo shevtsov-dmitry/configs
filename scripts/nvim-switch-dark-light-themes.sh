ce9885898add614172a13412d08d1aa222b27e22
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-02-25 17:16:20 +0300

    improve theme switcher for neovim

--- scripts/nvim-switch-dark-light-themes.sh
+++ scripts/nvim-switch-dark-light-themes.sh
@@ -1,18 +1,24 @@
+#!/bin/sh
+
 FILE_PATH=~/.config/nvim/lua/config/options.lua
 
 LIGHT_CONFIG='vim.go.background = "light"'
 DARK_CONFIG='vim.go.background = "dark"'
 
-grep_light=$(grep "$FILE_PATH" -r -o -e "$LIGHT_CONFIG")
-grep_dark=$(grep "$FILE_PATH" -r -o -e "$DARK_CONFIG")
+grep_light=$(grep "$LIGHT_CONFIG" "$FILE_PATH")
+grep_dark=$(grep "$DARK_CONFIG" "$FILE_PATH")
 
-if [[ "$grep_light" == "$LIGHT_CONFIG" ]]; then
-	sed -i "s/$LIGHT_CONFIG/$DARK_CONFIG/" $FILE_PATH
+if [ -n "$grep_light" ]; then
+	sed -i "s/$LIGHT_CONFIG/$DARK_CONFIG/" "$FILE_PATH"
+	FILE_PATH=~/.config/nvim/lua/plugins/colorscheme.lua
+	sed -i "s/colorscheme = \"melange\"/colorscheme = \"onedark\"/" "$FILE_PATH"
 	echo "dark mode set"
-elif [[ "$grep_dark" == "$DARK_CONFIG" ]]; then
-	sed -i "s/$DARK_CONFIG/$LIGHT_CONFIG/" $FILE_PATH
+elif [ -n "$grep_dark" ]; then
+	sed -i "s/$DARK_CONFIG/$LIGHT_CONFIG/" "$FILE_PATH"
+	FILE_PATH=~/.config/nvim/lua/plugins/colorscheme.lua
+	sed -i "s/colorscheme = \"onedark\"/colorscheme = \"melange\"/" "$FILE_PATH"
 	echo "light mode set"
 else
-	echo "ERROR: Neovim. couldn't find the path to file.
-    Path set in the script: $FILE_PATH"
+	echo "ERROR: Neovim. couldn't find the path to file."
+	echo "Path set in the script: $FILE_PATH"
 fi
