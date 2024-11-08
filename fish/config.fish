aa53ad002216f4290dafae5b2dac44841654c537
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-11-08 09:24:21 +0300

    update recent and deprecated confiugs

--- fish/config.fish
+++ fish/config.fish
@@ -47,7 +47,7 @@ end
 
 ## Starship prompt
 if status --is-interactive
-    source ("/usr/bin/starship" init fish --print-full-init | psub)
+    source ("/usr/local/bin/starship" init fish --print-full-init | psub)
 end
 
 ## Functions
