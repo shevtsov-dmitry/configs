6eb40e91a10970ffe386a9e820e41656d92f465a
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-11-11 12:22:36 +0300

    astonishing i3 setup with hardware monitor

---
+++ i3/scripts/toggle-theme.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get the current theme
+current_theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
+
+# Define the light and dark themes
+light_theme="Adwaita"
+dark_theme="Adwaita-dark"
+
+# Toggle the theme
+if [[ "$current_theme" == *"$light_theme"* ]]; then
+    gsettings set org.gnome.desktop.interface gtk-theme "$dark_theme"
+else
+    gsettings set org.gnome.desktop.interface gtk-theme "$light_theme"
+fi
