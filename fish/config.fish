db1ede95fc1653c2df4876140b0c5f71905df555
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-11-13 10:49:02 +0300

    update deprecated hyprland declaration of decoration

--- fish/config.fish
+++ fish/config.fish
@@ -47,7 +47,7 @@ end
 
 ## Starship prompt
 if status --is-interactive
-    source ("/usr/local/bin/starship" init fish --print-full-init | psub)
+    source (starship init fish --print-full-init | psub)
 end
 
 ## Functions
