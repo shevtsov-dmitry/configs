29687def8bc7f321f42817334a31285aa2070243
shevtsov-dmitry <shadowstakeus28@gmail.com>
2026-05-19 12:03:35 +0300

    hyprland move monitors and autostart

---
+++ hypr/monitors.lua
@@ -0,0 +1,11 @@
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+	output = "DP-2",
+	mode = "3840x2160@160",
+	position = "0x0",
+	scale = "1.5",
+})
