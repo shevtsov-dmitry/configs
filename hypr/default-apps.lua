eced21e68ee5c020a5c1d982d3196502f74b78ed
shevtsov-dmitry <shadowstakeus28@gmail.com>
2026-05-19 12:12:58 +0300

    hyprland extend default apps with launchers config

--- hypr/default-apps.lua
+++ hypr/default-apps.lua
@@ -1,6 +1,8 @@
 return {
 	terminal = "alacritty",
 	fileManager = "thunar",
-	capturing = "grim",
-	appLauncher = "rofi",
+	capturing = "grim -g $(slurp) - | swappy -f -",
+	appLauncher = "rofi -show combi -modi window,drun,combi -combi-modi window,drun",
+	idlehandler = "swayidle -w timeout 300 'swaylock -f -c 000000' before-sleep 'swaylock -f -c 000000'",
+	browser = "google-chrome-stable",
 }
