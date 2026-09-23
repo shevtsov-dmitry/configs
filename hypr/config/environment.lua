0d82e678c6214bb876e2e185a29b1e2fcd3af2da
shevtsov-dmitry <shadowstakeus28@gmail.com>
2026-09-23 18:43:43 +0300

    hyprland fix XDG desktop env

--- hypr/config/environment.lua
+++ hypr/config/environment.lua
@@ -5,4 +5,4 @@ hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 hl.env("QT_STYLE_OVERRIDE", "kvantum")
 
-hl.env("XDG_CURRENT_DESKTOP", "KDE")
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
