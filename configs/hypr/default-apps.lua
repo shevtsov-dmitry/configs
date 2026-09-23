17404af1d489fbcf813b76d64bf0dfffde7a26d0
shevtsov-dmitry <shadowstakeus28@gmail.com>
2026-09-23 18:44:29 +0300

    fix exclude duplicated useless files

--- configs/hypr/default-apps.lua
+++
@@ -1,9 +0,0 @@
-return {
-	terminal = "alacritty",
-	fileManager = "thunar",
-	capturing = "grim -g $(slurp) - | swappy -f -",
-	appLauncher = "rofi -show combi -modi window,drun,combi -combi-modi window,drun",
-	idlehandler = "swayidle -w timeout 300 'swaylock -f -c 000000' before-sleep 'swaylock -f -c 000000'",
-	browser = "google-chrome-stable",
-	clipboard = "cliphist list | wofi --dmenu | cliphist decode | wl-copy",
-}
--- fish/tempo-envs.fish
+++
@@ -1,3 +0,0 @@
-set -x POSTGRES_USER shd
-set -x POSTGRES_PASSWORD 123123
-set -x MYENV "something about you girl"
